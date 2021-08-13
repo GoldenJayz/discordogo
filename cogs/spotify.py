6e14d05041be237225c021abf8445061bc0fe856
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2021-08-13 10:26:13 -0400

    Update spotify.py

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -18,21 +18,10 @@ class spotify(commands.Cog):
 
     async def fetchdata(self, type: str, args: str) -> str:
         data: str = ""
-        if type == "artist":
+        spotifytypes = ["artist", "track", "album", "playlist"]
+        if type in spotifytypes:
             results = sp.search(q=args, limit=10, type=type)
-            for idx, trac in enumerate(results['artists']['items']):
-                data += f"{idx + 1}. {trac['name']}\n"
-        elif type == "track":
-            results = sp.search(q=args, limit=10, type=type)
-            for idx, trac in enumerate(results['tracks']['items']):
-                data += f"{idx + 1}. {trac['name']}\n"
-        elif type == "album":
-            results = sp.search(q=args, limit=10, type=type)
-            for idx, trac in enumerate(results['albums']['items']):
-                data += f"{idx + 1}. {trac['name']}\n"
-        elif type == "playlist":
-            results = sp.search(q=args, limit=10, type=type)
-            for idx, trac in enumerate(results['playlists']['items']):
+            for idx, trac in enumerate(results[f'{type}s']['items']):
                 data += f"{idx + 1}. {trac['name']}\n"
         elif type == "movie":
             search = tmbd.Search()
@@ -44,28 +33,32 @@ class spotify(commands.Cog):
 
     @commands.command()
     async def search(self, ctx, type: str, *, args: str):
-        embed = discord.Embed(
-            title="Search API",
-            timestamp=datetime.datetime.utcnow(),
-            colour=0x00ff08,
-            description=await self.fetchdata(type, args)
-        )
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-        await ctx.send(embed=embed)
+        if await self.fetchdata(type, args) == "":
+            await ctx.send("Please provide a valid search type!")
+        else:
+            embed = discord.Embed(
+                title="Search API",
+                timestamp=datetime.datetime.utcnow(),
+                colour=0x00ff08,
+                description=await self.fetchdata(type, args)
+            )
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=f"{ctx.author.avatar_url}")
+            await ctx.send(embed=embed)
 
     @search.error
     async def searcherror(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             embed = discord.Embed(
-            title="Spotify Search API",
+            title="Search Command",
             timestamp=datetime.datetime.utcnow(),
-            colour=0x00ff08,
-            description="Example: -spotifysearch track Despacito"
+            colour=0x00ff08
             )
-            embed.add_field(name="1st Argument", value="artist|album|track|playlist|movie")
-            embed.add_field(name="2nd Argument", value="The search query", inline=False)
+            embed.add_field(name="Argument 1", value="artist | album | track | playlist | movie")
+            embed.add_field(name="Argument 2", value="<search query>", inline=False)
+            embed.set_image(url="https://cdn.discordapp.com/attachments/742148980682784793/875745757418885160/unknown.png")
             embed.set_footer(text=f"Requested by {ctx.author}", icon_url=f"{ctx.author.avatar_url}")
             await ctx.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(spotify(client))
